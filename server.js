876ee25b69559461141cd2577552a2a40dde3b7b
javari <javaridesign@gmai.com>
2026-03-27 01:59:27 +0200

    Looking to check for error on browser console

--- server.js
+++ server.js
@@ -9,18 +9,40 @@
 
 
 
-const app = require('./app');
+// const app = require('./app');
 
-// Only listen when running locally, not on Vercel
-if (process.env.NODE_ENV !== 'production') {
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-        console.log('server has started...');
-    });
-}
+// // Only listen when running locally, not on Vercel
+// if (process.env.NODE_ENV !== 'production') {
+//     const port = process.env.PORT || 3000;
+//     app.listen(port, () => {
+//         console.log('server has started...');
+//     });
+// }
 
-module.exports = app;
+// module.exports = app;
 
 // app.listen(port, () => {
 //     console.log('server has started...');
 // })
+
+
+try {
+    const app = require('./app');
+    
+    if (process.env.NODE_ENV !== 'production') {
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log('server has started...');
+        });
+    }
+    
+    module.exports = app;
+} catch (err) {
+    module.exports = (req, res) => {
+        res.status(500).json({
+            crashed: true,
+            error: err.message,
+            stack: err.stack
+        });
+    };
+}
