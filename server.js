f313e1b1ab08348918a58596b210af8e5356bdef
javari <javaridesign@gmai.com>
2026-03-27 00:42:24 +0200

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 
 // const mongoose = require('mongoose');
-const app = require('./app');
+// const app = require('./app');
 
 // console.log(process.env);
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log('server has started...');
-})
+
+
+
+const app = require('./app');
+
+// Only listen when running locally, not on Vercel
+if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log('server has started...');
+    });
+}
+
+module.exports = app;
+
+// app.listen(port, () => {
+//     console.log('server has started...');
+// })
