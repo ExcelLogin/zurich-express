4c22bbbf27f86270dc949748f45123ad0361c824
javari <javaridesign@gmai.com>
2026-03-28 16:27:59 +0200

    redeploying

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,29 +1,5 @@
 
 
-// const mongoose = require('mongoose');
-
-// let isConnected = false;
-
-// const connectDB = async () => {
-//     if (isConnected) return; // reuse existing connection
-
-//     try {
-//         const db = await mongoose.connect(process.env.DATABASE_URI, {
-//             serverSelectionTimeoutMS: 10000,
-//             socketTimeoutMS: 45000,
-//         });
-
-//         isConnected = db.connections[0].readyState === 1;
-//         console.log('MongoDB connected');
-//     } catch (err) {
-//         console.error('MongoDB connection error:', err);
-//         throw err;
-//     }
-// };
-
-// module.exports = connectDB;
-
-
 const mongoose = require('mongoose');
 
 let isConnected = false;
@@ -32,13 +8,13 @@ const connectDB = async () => {
     if (isConnected) return; // reuse existing connection
 
     try {
-        const conn = await mongoose.connect(process.env.DATABASE_URI, {
+        const db = await mongoose.connect(process.env.DATABASE_URI, {
             serverSelectionTimeoutMS: 10000,
             socketTimeoutMS: 45000,
         });
 
-        isConnected = true;
-        console.log(`MongoDB connected: ${conn.connection.host}`);
+        isConnected = db.connections[0].readyState === 1;
+        console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err);
         throw err;
@@ -46,3 +22,27 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+
+// const mongoose = require('mongoose');
+
+// let isConnected = false;
+
+// const connectDB = async () => {
+//     if (isConnected) return; // reuse existing connection
+
+//     try {
+//         const conn = await mongoose.connect(process.env.DATABASE_URI, {
+//             serverSelectionTimeoutMS: 10000,
+//             socketTimeoutMS: 45000,
+//         });
+
+//         isConnected = true;
+//         console.log(`MongoDB connected: ${conn.connection.host}`);
+//     } catch (err) {
+//         console.error('MongoDB connection error:', err);
+//         throw err;
+//     }
+// };
+
+// module.exports = connectDB;
