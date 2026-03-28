3cbd85b81273f20025e24ac1ac91e4daae8effc7
javari <javaridesign@gmai.com>
2026-03-28 16:14:02 +0200

    redeployed for mongo db persist connection

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,5 +1,29 @@
 
 
+// const mongoose = require('mongoose');
+
+// let isConnected = false;
+
+// const connectDB = async () => {
+//     if (isConnected) return; // reuse existing connection
+
+//     try {
+//         const db = await mongoose.connect(process.env.DATABASE_URI, {
+//             serverSelectionTimeoutMS: 10000,
+//             socketTimeoutMS: 45000,
+//         });
+
+//         isConnected = db.connections[0].readyState === 1;
+//         console.log('MongoDB connected');
+//     } catch (err) {
+//         console.error('MongoDB connection error:', err);
+//         throw err;
+//     }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require('mongoose');
 
 let isConnected = false;
@@ -8,13 +32,13 @@ const connectDB = async () => {
     if (isConnected) return; // reuse existing connection
 
     try {
-        const db = await mongoose.connect(process.env.DATABASE_URI, {
+        const conn = await mongoose.connect(process.env.DATABASE_URI, {
             serverSelectionTimeoutMS: 10000,
             socketTimeoutMS: 45000,
         });
 
-        isConnected = db.connections[0].readyState === 1;
-        console.log('MongoDB connected');
+        isConnected = true;
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (err) {
         console.error('MongoDB connection error:', err);
         throw err;
